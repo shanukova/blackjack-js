56b2943f8633cdbc4e35c48377c63f140871719e
Victoria Shanukova <vshanukova@gmail.com>
2021-09-23 16:56:45 +0100

    declared dock values

---
+++ index.js
@@ -0,0 +1,17 @@
+// Step 1: Decalare variables for deck and dealer and player hands
+const SUITS = ["♠", "♣", "♥", "♦"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
