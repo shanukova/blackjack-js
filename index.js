3430ee15c52f94e5412e0e1f28d6b664e716ab3c
Victoria Shanukova <vshanukova@gmail.com>
2021-09-24 13:20:15 +0100

    decalared variables for deck and dealer and player hands

--- index.js
+++ index.js
@@ -118,3 +118,22 @@ var finishGame = (result) => {
     }
   }, 2000);
 }
+// Step 1: Decalare variables for deck and dealer and player hands
+const SUITS = ["♠", "♣", "♥", "♦"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
+var dealerHand = [];
+var playerHand = [];
